d20777a0096c475d3874b6c462b62ec5085c2cd3
zhaofeng-shu33 <616545598@qq.com>
2019-09-30 22:19:30 +0800

    update readme

--- counting.cpp
+++ counting.cpp
@@ -1,10 +1,10 @@
 #include "counting.h"
 
-int triangle_count_given_edge(const ListGraph& G, const ListGraph::Edge& e){
+int triangle_count_given_edge(const ListGraph& G, const ListGraph::Edge& e, const ArcLookUp<ListGraph>& look_up){
     ListGraph::Node u = G.u(e);
     ListGraph::Node v = G.v(e);
     int t_count = 0;
-    ArcLookUp<ListGraph> look_up(G);
+    
     for(ListGraph::IncEdgeIt e1(G, u); e1 != INVALID; ++e1){
         ListGraph::Node u_target = G.v(e1);
         ListGraph::Arc a = look_up(u_target, v);
@@ -14,9 +14,10 @@ int triangle_count_given_edge(const ListGraph& G, const ListGraph::Edge& e){
 }
 
 int triangle_count(const ListGraph& G){
+    ArcLookUp<ListGraph> look_up(G);
     int triangle_sum = 0;
     for(ListGraph::EdgeIt e(G); e != INVALID; ++e){
-        triangle_sum += triangle_count_given_edge(G, e);
+        triangle_sum += triangle_count_given_edge(G, e, look_up);
     }
     return triangle_sum / 3;
 }
--- counting.h
+++ counting.h
@@ -1,4 +1,3 @@
 #include <lemon/list_graph.h>
 using namespace lemon;
-int triangle_count_given_edge(const ListGraph& G, const ListGraph::Edge& e);
 int triangle_count(const ListGraph& G);
