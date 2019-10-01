46352d8e678f23b62efd430f7398513989bf76df
zhaofeng-shu33 <616545598@qq.com>
2019-10-01 10:34:01 +0800

    ignore all things related with build

--- config.h
+++ config.h
@@ -1,3 +1,11 @@
-#include <lemon/list_graph.h>
+#if SMARTGRAPH
+    #include <lemon/smart_graph.h>
+#else
+    #include <lemon/list_graph.h>
+#endif
 using namespace lemon;
-typedef ListGraph Graph;
+#if SMARTGRAPH
+    typedef SmartGraph Graph;
+#else
+    typedef ListGraph Graph;
+#endif
