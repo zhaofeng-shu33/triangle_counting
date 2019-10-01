b458bb95910ee8399b04ead11842ade3d4ef7992
zhaofeng-shu33 <616545598@qq.com>
2019-10-01 10:07:49 +0800

    add time counting preprocessor switch

--- counting.cpp
+++ counting.cpp
@@ -1,7 +1,11 @@
 #include "counting.h"
 #if VERBOSE
 #include <iostream>
-#endif        
+#endif
+#if TIMECOUNTING
+#include <iostream>
+#include <chrono>
+#endif
 int triangle_count_given_edge(const Graph& G, const Graph::Edge& e, const ArcLookUp<Graph>& look_up){
     Graph::Node u = G.u(e);
     Graph::Node v = G.v(e);
@@ -20,7 +24,10 @@ unsigned long triangle_count(const Graph& G){
     unsigned long triangle_sum = 0;
 #if VERBOSE
     int iteration_cnt = 0;
-#endif        
+#endif
+#if TIMECOUNTING
+    std::chrono::system_clock::time_point start_time = std::chrono::system_clock::now();
+#endif    
     for(Graph::EdgeIt e(G); e != INVALID; ++e){
         triangle_sum += triangle_count_given_edge(G, e, look_up);
 #if VERBOSE
@@ -28,7 +35,13 @@ unsigned long triangle_count(const Graph& G){
         std::cout << iteration_cnt << " edges processed" << std::endl;
     iteration_cnt ++;
 #endif
-    }    
+    }
+#if TIMECOUNTING
+    std::chrono::system_clock::time_point end_time = std::chrono::system_clock::now();
+    std::chrono::system_clock::duration dtn = end_time - start_time;
+    float time_used = std::chrono::duration_cast<std::chrono::milliseconds>(dtn).count()/1000.0;
+    std::cout << "Time used: " << time_used << "s" << std::endl;
+#endif        
     return triangle_sum / 3;
 }
 
