72ee36efd974204f75c6da45fcfa688abbb5e2e9
zhaofeng-shu33 <616545598@qq.com>
2019-10-09 19:35:57 +0800

    fix index not found problem

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,6 +44,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+master_doc = 'index'
 html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
