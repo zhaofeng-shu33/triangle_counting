65d4b4abe311599bd446908a35e655bc073f15de
zhaofeng-shu33 <616545598@qq.com>
2019-10-09 16:46:47 +0800

    fix lint problem

--- lemontc/counting.h
+++ lemontc/counting.h
@@ -15,7 +15,7 @@ namespace lemon {
     int collect_degree_info(const Graph& G,
             std::vector<int>* degree_list, int node_size);
     //! use node iteration method to count the triangle
-    //! \return the exact number of triangle in the graph            
+    //! \return the exact number of triangle in the graph
     int64_t triangle_count_vertex_iteration(const Graph& G,
                 const std::vector<int>& degree_list, int max_degree);
-}
+}  // namespace lemon
--- lemontc/io.h
+++ lemontc/io.h
@@ -12,7 +12,7 @@ namespace lemon {
     //! construct the static graph from given arc list
     void construct_graph_from_arclist(Graph* G,
             const std::vector<std::pair<int, int> >& arcs, int node_size);
-    //! read the binary graph file and initialize the arcs data structure        
+    //! read the binary graph file and initialize the arcs data structure
     std::pair<int, int> read_binfile_to_arclist(const char* file_name,
                             std::vector<std::pair<int, int>>* arcs);
 }
